c4479b7a115c415a3ea5c772786f03ec264c8c4c
Vladyslava <c.stercho.vladyslava@student.uzhnu.edu.ua>
2026-06-15 22:04:49 +0300

    Налаштування деплою на Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,4 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->booted(function (): void {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+    })
+    ->create();
